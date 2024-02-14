4c97718966da9f4a6dad763c44aadaf99f51edd4
Damian <damianpietron007@gmail.com>
2024-02-14 01:18:16 +0100

    add admin and models

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Movies(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    active = models.BooleanField(default=True)
+        
+    def __str__(self):
+        return self.name
